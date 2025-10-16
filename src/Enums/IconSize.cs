17ba3603ad306b2397c0f9d21b4cd0bb80a9a146
Jake Soenneker <jake@soenneker.com>
2025-10-16 12:35:07 -0500

    iconsize mod

--- src/Enums/IconSize.cs
+++ src/Enums/IconSize.cs
@@ -11,13 +11,13 @@ public sealed partial class IconSize
     public static readonly IconSize Small = new("fa-sm");
     public static readonly IconSize Large = new("fa-lg");
 
-    public static readonly IconSize IsTwoX = new("fa-2x");
-    public static readonly IconSize IsThreeX = new("fa-3x");
-    public static readonly IconSize IsFourX = new("fa-4x");
-    public static readonly IconSize IsFiveX = new("fa-5x");
-    public static readonly IconSize IsSixX = new("fa-6x");
-    public static readonly IconSize IsSevenX = new("fa-7x");
-    public static readonly IconSize IsEightX = new("fa-8x");
-    public static readonly IconSize IsNineX = new("fa-9x");
-    public static readonly IconSize IsTenX = new("fa-10x");
+    public static readonly IconSize Is2x = new("fa-2x");
+    public static readonly IconSize Is3x = new("fa-3x");
+    public static readonly IconSize Is4x = new("fa-4x");
+    public static readonly IconSize Is5x = new("fa-5x");
+    public static readonly IconSize Is6x = new("fa-6x");
+    public static readonly IconSize Is7x = new("fa-7x");
+    public static readonly IconSize Is8x = new("fa-8x");
+    public static readonly IconSize Is9x = new("fa-9x");
+    public static readonly IconSize Is10x = new("fa-10x");
 }
