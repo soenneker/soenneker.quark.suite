34c7c8cb9aa2b22e83e0d47b7949da633073a432
Jake Soenneker <jake@soenneker.com>
2025-12-15 14:08:15 -0600

    Centered, and ColumnSize

--- src/Components/Overlays/Modals/Abstract/IModalContent.cs
+++ src/Components/Overlays/Modals/Abstract/IModalContent.cs
@@ -5,5 +5,9 @@ namespace Soenneker.Quark;
 /// </summary>
 public interface IModalContent : IElement
 {
+    /// <summary>
+    /// Gets or sets whether the modal content should be centered.
+    /// </summary>
+    bool Centered { get; set; }
 }
 
