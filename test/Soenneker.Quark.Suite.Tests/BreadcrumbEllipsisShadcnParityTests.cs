f038f14cba039e9966cbb1c5b77d5b55d7bf7919
Jake Soenneker <jake@soenneker.com>
2026-04-28 17:52:33 -0500

    bradix

--- test/Soenneker.Quark.Suite.Tests/BreadcrumbEllipsisShadcnParityTests.cs
+++ test/Soenneker.Quark.Suite.Tests/BreadcrumbEllipsisShadcnParityTests.cs
@@ -1,5 +1,7 @@
 using AwesomeAssertions;
 using Bunit;
+using System;
+using System.IO;
 
 
 namespace Soenneker.Quark.Suite.Tests;
@@ -20,5 +22,29 @@ public sealed partial class RenderedShadcnParityTests
         classes.Should().Contain("[&>svg]:size-4");
         classes.Should().NotContain("size-9");
         classes.Should().NotContain("q-breadcrumb-ellipsis");
+    }
+
+    [Test]
+    public void Breadcrumbs_collapsed_demo_uses_dropdown_trigger_for_ellipsis()
+    {
+        var source = File.ReadAllText(Path.Combine(GetSuiteRootForBreadcrumbEllipsis(), "test", "Soenneker.Quark.Suite.Demo", "Pages", "Components", "Breadcrumbs.razor"));
+        var collapsedStart = source.IndexOf("Title=\"Collapsed\"", StringComparison.Ordinal);
+        var linkComponentStart = source.IndexOf("Title=\"Link Component\"", StringComparison.Ordinal);
+        var collapsedDemo = source[collapsedStart..linkComponentStart];
+
+        collapsedDemo.Should().Contain("<Dropdown>");
+        collapsedDemo.Should().Contain("<DropdownToggle AsChild=\"true\">");
+        collapsedDemo.Should().Contain("<BreadcrumbEllipsis />");
+        collapsedDemo.Should().Contain("Toggle menu");
+    }
+
+    private static string GetSuiteRootForBreadcrumbEllipsis()
+    {
+        var directory = AppContext.BaseDirectory;
+
+        while (!File.Exists(Path.Combine(directory, "Soenneker.Quark.Suite.slnx")))
+            directory = Directory.GetParent(directory)!.FullName;
+
+        return directory;
     }
 }
