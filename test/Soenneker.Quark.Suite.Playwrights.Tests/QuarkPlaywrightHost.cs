a39e64e9aea12ab5217b2122581513b13216bf4c
Jake Soenneker <jake@soenneker.com>
2026-06-05 10:17:01 -0500

    misc

--- test/Soenneker.Quark.Suite.Playwrights.Tests/QuarkPlaywrightHost.cs
+++ test/Soenneker.Quark.Suite.Playwrights.Tests/QuarkPlaywrightHost.cs
@@ -1,11 +1,23 @@
 using Soenneker.Playwrights.TestEnvironment.Options;
 using Soenneker.Playwrights.TestHosts;
+using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Soenneker.Quark.Suite.Playwrights.Tests;
 
 public sealed class QuarkPlaywrightHost : PlaywrightHostedTestHost
 {
+    public override async ValueTask DisposeAsync()
+    {
+        try
+        {
+            await base.DisposeAsync();
+        }
+        catch (ObjectDisposedException ex) when (ex.ObjectName == "System.Threading.SemaphoreSlim")
+        {
+        }
+    }
 
     protected override PlaywrightTestHostOptions CreateOptions()
     {
